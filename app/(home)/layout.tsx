8b32d71aee9828182a6c5572e87c1e0c64168fd6
root <anthonydezer@proton.me>
2025-12-22 14:18:02 +0100

    fixes

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,16 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/docs/guides/installation');
+  }, [router]);
+
+  return;
 }
