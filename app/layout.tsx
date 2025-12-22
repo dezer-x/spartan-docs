53ec3701b331fe5a833b8ce6caab16df29dc2311
root <anthonydezer@proton.me>
2025-12-22 14:26:55 +0100

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({
   subsets: ['latin'],
 });
+
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
